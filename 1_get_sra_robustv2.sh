ab80f4ce3edf4f87c0b28108ff7eba8ab70a9aa2
Abid Al Reza <abidgen69@gmail.com>
2022-09-27 18:01:22 -0400

    Update 1_get_sra_robustv2.sh

--- 1_get_sra_robustv2.sh
+++ 1_get_sra_robustv2.sh
@@ -3,13 +3,15 @@
 #activate right env
 #conda activate msa
 
+# get SraRunTable.txt from SRA database
+
 #get input data (uniprot ids)
 SRA=$(tail -n +2 SraRunTable.txt | cut -d ',' -f 1)
 
 #This is a loop
 for i in $SRA
 	do
-		prefetch $i
+		prefetch $i ## prefetch downloads SRA file
 		if [ -f $i.fastq.gz ]
                 then
                         echo "$i already converted"
